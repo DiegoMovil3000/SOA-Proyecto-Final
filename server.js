7cccc065fc7b9bbb29773674bc28b8b5a90b8768
23020200015-abe <23020200015@uppenjamo.edu.mx>
2026-08-13 12:24:56 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+// --- ESTADO EN MEMORIA ---
+let boteAbierto = false;
+let checkpoints = [
+  { id: "1", nombre: "Colonia Centro", direccion: "Av. Independencia #100", completado: true, hora: "07:15" },
+  { id: "2", nombre: "Jardines del Sol", direccion: "Calle Rosal #45", completado: true, hora: "07:45" },
+  { id: "3", nombre: "Las Palmas", direccion: "Blvd. Las Palmas #200", completado: false, hora: "" },
+  { id: "4", nombre: "Colonia Moderna", direccion: "Calle Reforma #88", completado: false, hora: "" }
+];
